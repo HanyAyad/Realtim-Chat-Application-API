1e2234547aaca38c7a394c1e94824106e7406ff4
Hany Eskandar <52006612+HanyAyad@users.noreply.github.com>
2023-02-20 12:54:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: `${process.env.HOST}`,
+    origin: `${process.env.REAC_APP_HOST}`,
     credentials: true,
   },
 });
