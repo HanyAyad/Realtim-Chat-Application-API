dbce09415ae6a10127e1f7f46147ecea7652470a
Hany Eskandar <52006612+HanyAyad@users.noreply.github.com>
2023-02-20 10:33:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: `${process.env.HOST}`,
     credentials: true,
   },
 });
